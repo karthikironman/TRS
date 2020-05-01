3a84772c1095398797b6ffd2b0b67ee80758ab43
karthikironman <karthik.selvaraj@diatoz.com>
2020-05-01 21:49:48 +0530

    base_code

---
+++ src/components/pages/addtonewsite.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Add to new site</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
